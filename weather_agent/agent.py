d36c6423e783c75c80a052fba5d486f20c296198
csaba <csabak@zillowgroup.com>
2025-10-16 20:41:56 +0000

    gsearch tool added

--- weather_agent/agent.py
+++ weather_agent/agent.py
@@ -1,5 +1,6 @@
 from google.adk.agents.llm_agent import Agent
 from google.adk.agents import SequentialAgent
+from google.adk.tools.google_search_tool import google_search
 
 weather_agent_sequential = Agent(
     model='gemini-2.5-flash',
@@ -8,11 +9,16 @@ weather_agent_sequential = Agent(
     instruction="answer the user's question",
 )
 
-root_agent = SequentialAgent(
-    name="root_agent",
-    sub_agents=[weather_agent_sequential],
-    description="Just pass the workflow to sequential",
+google_search_agent = Agent(
+    model='gemini-2.5-flash',
+    name='google_search_agent',
+    description='A helpful assistant for user questions that require searching the web.',
+    instruction="answer the user's question using the google search tool",
+    tools=[google_search],
 )
 
-
-
+root_agent = SequentialAgent(
+    name="root_agent",
+    sub_agents=[weather_agent_sequential, google_search_agent],
+    description="Just pass the workflow to sequential",
+)
